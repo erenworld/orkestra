f8a0eb34a89376e8a4ad948ac0bf2ea23817b3d6
erenworld <erenxturkoglu@gmail.com>
2025-11-14 07:47:12 +0100

    Initialize Manager struct

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,19 @@
 package manager
+
+import (
+	"orkestra/task"
+	"fmt"
+
+	"github.com/golang-collections/collections/queue"
+ 	"github.com/google/uuid"
+)
+
+type Manager struct {
+	Pending			queue.Queue
+	TaskDb  		map[string][]*task.Task
+	EventDb 		map[string][]*task.TaskEvent
+	Workers			[]string
+	WorkerTaskMap 	map[string][]uuid.UUID
+	TaskWorkerMap 	map[uuid.UUID]string
+}
+
