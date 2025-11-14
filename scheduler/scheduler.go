53b99493141e9ba38fe42be5ae3e6f17b736a731
erenworld <erenxturkoglu@gmail.com>
2025-11-14 07:56:30 +0100

    Add the skeleton of the Scheduler component

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,1 +1,12 @@
 package scheduler
+
+// Determine a set of candidate workers on which a task could run
+// Score the candidate workers from best to worst
+// Pick the worker with the best score
+
+// We use interface for polymorphism
+type Scheduler interface {
+	SelectCandidateNodes()
+	Select()
+	Pick()
+}
