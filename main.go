14e04ba056d7232e6ab07986494da50f10cbd0d0
erenworld <erenxturkoglu@gmail.com>
2025-11-16 12:56:09 +0100

    Add createContainer

--- main.go
+++ main.go
@@ -8,9 +8,36 @@ import (
 	"orkestra/worker"
 	"time"
 
+	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 )
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name: 	"test-container-1",
+		Image: 	"postgres:13",
+		Env:	[]string{
+			"POSTGRES_USER=orkestra",
+			"POSTGRES_PASSWORD=secret",
+		},
+	}
+
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+
+	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, c)
+	return &d, &result
+}
 
 func main() {
 	t := task.Task{
